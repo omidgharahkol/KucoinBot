11369e2d4c7c6a236e6d24181ef1be512eab7ebc
WALLEX <wallex@WALLEXs-MacBook-Pro-2.local>
2023-06-07 02:29:48 +0330

    modify

--- test.py
+++ test.py
@@ -1,17 +1,15 @@
-while True:
+last_value = ''
+
+while last_value != 'EndSpread':
     with open("file.txt") as file_in:
         lines = []
         for line in file_in:
             if line == 'EndSpread':
+                last_value = 'EndSpread'
                 break
             else:
-                symbol , target_price = line.strip('\n').split(',')
-                print(symbol,target_price)
+                symbol, target_price = line.strip('\n').split(',')
+                print(symbol, target_price)
 
-
-
-
-
-
-file_to_delete = open("file.txt",'w')
+file_to_delete = open("file.txt", 'w')
 file_to_delete.close()
