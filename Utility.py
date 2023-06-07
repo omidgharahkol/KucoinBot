0c72d0ca85ea72cee02313e2502a4186bd68cacc
WALLEX <wallex@WALLEXs-MacBook-Pro-2.local>
2023-06-07 16:28:00 +0330

    modifty

--- Utility.py
+++ Utility.py
@@ -1,15 +1,19 @@
 def add_price(price, add_number):
+    len_first = len(str(price))
+    print(len_first)
     number, decimal = str(float(price)).split('.')
     if decimal == '0':
         new_price = price + add_number
+        new_price = str(new_price)[:len_first]
         return float(new_price)
     else:
         count_decimal = len(decimal)
 
         new_price = price + add_number / (10 ** count_decimal)
+        new_price = float(str(new_price)[:len_first])
         return new_price
 
 
 if __name__ == '__main__':
-    c = add_price(10.612, -3)
-    print(c+4)
+    c = add_price(0.011192, -3)
+    print(c)
